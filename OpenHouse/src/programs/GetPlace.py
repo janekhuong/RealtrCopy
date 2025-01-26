8f969e6c212a437e4ffa252064909be3f64437e2
Noah Deschenes <73668501+Noutronix@users.noreply.github.com>
2025-01-25 21:14:19 -0500

    Update GetPlace.py

--- OpenHouse/src/programs/GetPlace.py
+++ OpenHouse/src/programs/GetPlace.py
@@ -1,26 +1,23 @@
 import requests
 from requests.structures import CaseInsensitiveDict
 import sys
-
+from urllib.parse import quote, urlencode
 
 api_key = "53bc05c2b55b484fabd3edb050912758"
 base_url = 'https://api.geoapify.com/v2/places'
 
 # Parameters
-limit = 5
+limit = 50
 
 
-categories = sys.argv[1]
-lat = sys.argv[2]
-long = sys.argv[3]
-range = sys.argv[4]
+categories = quote("activity,commercial,catering,education,entertainment,healthcare")
+lat = sys.argv[1]
+long = sys.argv[2]
+range = 1000
 filter_circle = 'circle:{lat},{long},{range}'
 
 # Build the URL with parameters
 url = f"{base_url}?categories={categories}&filter={filter_circle}&limit={limit}&apiKey={api_key}"
-
-
-#url = "https://api.geoapify.com/v2/places?categories=commercial.supermarket&filter=rect%3A10.716463143326969%2C48.755151258420966%2C10.835314015356737%2C48.680903341613316&limit=20&apiKey=53bc05c2b55b484fabd3edb050912758"
 
 headers = CaseInsensitiveDict()
 headers["Accept"] = "application/json"
