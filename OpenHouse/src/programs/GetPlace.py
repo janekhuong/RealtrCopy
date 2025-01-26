199d96a7b280bdf0bf6afdecd9d27096181bb7d3
Noah Deschenes <73668501+Noutronix@users.noreply.github.com>
2025-01-25 21:46:52 -0500

    Update GetPlace.py

--- OpenHouse/src/programs/GetPlace.py
+++ OpenHouse/src/programs/GetPlace.py
@@ -1,31 +1,45 @@
 import requests
 from requests.structures import CaseInsensitiveDict
 from urllib.parse import quote, urlencode
-import subprocess
-
-api_key = "53bc05c2b55b484fabd3edb050912758"
-base_url = 'https://api.geoapify.com/v2/places'
+import sys
+import json
 
 # Parameters
-limit = 50
+api_key = "53bc05c2b55b484fabd3edb050912758" 
 
+base_url = "https://api.geoapify.com/v1/geocode/search"
 
+#address = sys.argv[1]
+address = "100 rue St-Denis, Montreal, QC"
 
-coords = subprocess.run(["python", "./GetCoords"], capture_output=True, text=True)
-coords.stdout.splitlines()
+# Encode the text
+encoded_address = quote(address)
 
-
-categories = quote("activity,commercial,catering,education,entertainment,healthcare")
-lat = coords[1]
-long = coords[2]
-range = 1000
-filter_circle = 'circle:{lat},{long},{range}'
-
-# Build the URL with parameters
-url = f"{base_url}?categories={categories}&filter={filter_circle}&limit={limit}&apiKey={api_key}"
+# Construct the full URL
+full_url = f"{base_url}?text={encoded_address}&limit=1&apiKey={api_key}"
 
 headers = CaseInsensitiveDict()
 headers["Accept"] = "application/json"
+
+resp = requests.get(full_url, headers=headers)
+
+# Parse the JSON string into a Python dictionary
+data = json.loads(resp.text)
+
+# Extract the coordinates for all features
+coords = [feature['geometry']['coordinates'] for feature in data['features']]
+
+
+limit = 50
+categories = quote("catering,commercial,education,healthcare")
+base_url_places = 'https://api.geoapify.com/v2/places'
+lat = coords[0][0]
+long = coords[0][1]
+range = 1000
+filter_circle = f'circle:{lat},{long},{range}'
+
+# Build the URL with parameters
+url = f"{base_url_places}?categories={categories}&filter={filter_circle}&limit={limit}&apiKey={api_key}"
 
 resp = requests.get(url, headers=headers)
 
